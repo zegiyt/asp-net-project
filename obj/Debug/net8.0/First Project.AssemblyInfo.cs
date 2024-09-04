2b545b6c51f71d216abe222de45b95036e28e582
Zegi <66989221+zegiyt@users.noreply.github.com>
2024-09-04 11:33:45 +0800

    another tab

--- obj/Debug/net8.0/First Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/First Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0be9074509187721e6ca952a85968a5c80afbf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("First Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
